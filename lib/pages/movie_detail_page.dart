f014ad37cb4c736a578ce1967a96345cf4a4b496
dafm10 <dafm.10@gmail.com>
2022-01-14 21:43:41 -0500

    SliverAppBar 2

--- lib/pages/movie_detail_page.dart
+++ lib/pages/movie_detail_page.dart
@@ -6,17 +6,40 @@ class MovieDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff18162E),
       body: CustomScrollView(
         // physics: BouncingScrollPhysics(),
         slivers: [
           SliverAppBar(
+            backgroundColor: Color(0xff18162E),
             elevation: 0,
             expandedHeight: 200.0,
             floating: false,
             snap: false,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text("Hola"),
+              title: Text("Movies"),
+              background: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    "https://images.pexels.com/photos/2611686/pexels-photo-2611686.jpeg",
+                    fit: BoxFit.cover,
+                  ),
+                  DecoratedBox(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.center,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0x0018162E),
+                          Color(0xff18162E),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
               centerTitle: true,
             ),
           ),
